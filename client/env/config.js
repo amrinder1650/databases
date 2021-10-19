3eccad5568972052915e07367347c812837a4d03
Joseph <josephnahm1@gmail.com>
2021-10-18 20:12:55 -0700

    Copied Client directory

---
+++ client/env/config.js
@@ -0,0 +1,6 @@
+$.ajaxPrefilter(function (settings, _, jqXHR) {
+  jqXHR.setRequestHeader('Authorization', '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');
+});
+
+// Put your campus prefix here
+window.CAMPUS = 'hr-rpp';
---
+++ client/scripts/messageView.js
@@ -0,0 +1,20 @@
+// Whereas MessagesView controls the entire list of messages,
+// MessageView is responsible for rendering a single message.
+
+var MessageView = {
+  render: _.template(`
+      <!--
+      -->
+      <div class="chat">
+        <div
+          class="username <%= Friends.isFriend(username) ? 'friend' : '' %>"
+          data-username="<%- username %>">
+          <%- username %>
+        </div>
+        <div><%- text %></div>
+      </div>
+      <!--
+            -->
+    `)
+
+};
