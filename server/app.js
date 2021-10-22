8288c4a1db2d5cac2ca9d6e9352c4d2d7e39c95c
Joseph <josephnahm1@gmail.com>
2021-10-21 18:05:17 -0700

    Added Cors middleware to prevent cors error and installed sequelize. GOing to start refactoring with sequelize

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var db = require('./db');
+var cors = require('cors');
 
 
 // Middleware
@@ -18,6 +19,7 @@ app.set('port', 3000);
 // Logging and parsing
 app.use(morgan('dev'));
 app.use(parser.json());
+app.use(cors());
 
 // Set up our routes
 app.use('/classes', router);
